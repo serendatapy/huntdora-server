4119755d51a38d68262cbbc7733e10c75b3a94ac
serendatapy <arvaldivia@gmail.com>
2020-12-16 16:12:00 +0100

    refactor: moved all env files to one place

--- models/index.js
+++ models/index.js
@@ -1,27 +1,30 @@
 'use strict';
+
 const MongoClient = require('mongodb').MongoClient;
-const conf = require('../config/config.js');
+const dbName = process.env.DB_NAME;
+const dbUrl = process.env.DB_URL;
+
 const db = {};
 
-db.MongoClient = new MongoClient(conf.dbUrl, { useUnifiedTopology: true, useNewUrlParser: true });
+db.MongoClient = new MongoClient(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true });
 db.MongoClient.connect(err => {
   if (err) throw new Error(err);
-  db.conn = db.MongoClient.db(conf.dbName)
-  console.log(`Connected to the database '${conf.dbName}'`);
+  db.conn = db.MongoClient.db(dbName)
+  console.log(`Connected to the database '${dbName}'`);
   // perform actions on the collection object
   //client.close();
 });
 
 
 
-db.MongoClient = new MongoClient(conf.dbUrl);
+db.MongoClient = new MongoClient(dbUrl);
 
 
 
 db.MongoClient.connect((err) => {
   if (err) throw new Error(err);
-  db.conn = db.MongoClient.db(conf.dbName)
-  console.log(`Connected to the database '${conf.dbName}'`);
+  db.conn = db.MongoClient.db(dbName)
+  console.log(`Connected to the database '${dbName}'`);
 });
 
 module.exports = db;

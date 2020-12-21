8fe3df203be5686092f57984d5a217546fba4e05
serendatapy <arvaldivia@gmail.com>
2020-12-21 16:30:33 +0100

    feat: modified cors

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
-const clientOrigins = process.env.CLIENT_ORIGIN_URL;
+//const clientOrigins = process.env.CLIENT_ORIGIN_URL;
 const cors = require('cors');
 const {router} = require('./router');
 const connectDB = require('./models/');
 
-app.use(cors({ origin: clientOrigins }));
+//app.use(cors({ origin: clientOrigins }));
+app.use(cors());
 app.use(express.json());
 app.use(router);
 

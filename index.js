fe144cb8e64da49fe0a90fed2c03e0b793ac0f2c
serendatapy <arvaldivia@gmail.com>
2020-12-21 15:35:08 +0100

    feat: changed env variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.PORT;
 const clientOrigins = process.env.CLIENT_ORIGIN_URL;
 const cors = require('cors');
 const {router} = require('./router');
